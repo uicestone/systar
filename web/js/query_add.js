6061e6c5e104acb6944fb2e31a87452c6c3df4ef
Uice Stone <uicestone@gmail.com>
2012-09-18 17:57:56 +0800

    简单版咨询页面

--- web/js/query_add.js
+++ web/js/query_add.js
@@ -3,7 +3,9 @@ $(document).ready(function(){
 	$('select[name="source[type]"]').change(function(){
 		$('span#source_detail').empty();
 		if(jQuery.inArray($(this).val(),['其他网络','媒体','老客户介绍','中介机构介绍','其他'])>-1){
-			$('span#source_detail').html('<input type="text" name="source[detail]" style="width:49%" />');
+			$('[name="source[detail]"]').removeAttr('disabled').show();
+		}else{
+			$('[name="source[detail]"]').hide().attr('dssabled','disabled');
 		}
-	});
+	}).change();
 });
