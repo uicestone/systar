166537ca12af27f1801e285743a2447839f5fdd3
陆秋石 <uicestone@gmail.com>
2012-11-06 16:34:29 +0800

    修复分页显示错误

--- application/views/pagination.php
+++ application/views/pagination.php
@@ -1,4 +1,4 @@
-<?=option('list/start')+1?>-<?=option('list/start')+option('list/items')+1?>/<?=option('list/rows')?>
+<?=option('list/start')+1?>-<?=option('list/rows')<option('list/items')?option('list/rows'):option('list/start')+option('list/items')?>/<?=option('list/rows')?>
 <button type="button" class="nav" onclick="post('firstPage',true)"<?if(option('list/start')==0) echo 'disabled="disabled"'?>>&lt;&lt;</button>
 <button type="button" class="nav" onclick="post('previousPage',true)"<?if(option('list/start')==0) echo 'disabled="disabled"'?>>&nbsp;&lt;&nbsp;</button>
 <button type="button" class="nav" onclick="post('nextPage',true)"<?if(option('list/start')+option('list/items')>=option('list/rows')) echo 'disabled="disabled"'?>>&nbsp;&gt;&nbsp;</button>
