21e3e400cea5a3d887d6606f9efa35ba030db44f
陆秋石 <uicestone@gmail.com>
2012-11-12 20:25:29 +0800

    Netbeans下CI框架的自动完成

---
+++ _doc/CI_Autocomplete.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+* @property SS_DB_active_record $db
+* @property SS_DB_forge $dbforge
+* @property SS_Benchmark $benchmark
+* @property SS_Calendar $calendar
+* @property SS_Cart $cart
+* @property SS_Config $config
+* @property SS_Controller $controller
+* @property SS_Email $email
+* @property SS_Encrypt $encrypt
+* @property SS_Exceptions $exceptions
+* @property SS_Form_validation $form_validation
+* @property SS_Ftp $ftp
+* @property SS_Hooks $hooks
+* @property SS_Image_lib $image_lib
+* @property SS_Input $input
+* @property SS_Language $language
+* @property SS_Loader $load
+* @property SS_Log $log
+* @property SS_Model $model
+* @property SS_Output $output
+* @property SS_Pagination $pagination
+* @property SS_Parser $parser
+* @property SS_Profiler $profiler
+* @property SS_Router $router
+* @property SS_Session $session
+* @property SS_Sha1 $sha1
+* @property SS_Table $table
+* @property SS_Trackback $trackback
+* @property SS_Typography $typography
+* @property SS_Unit_test $unit_test
+* @property SS_Upload $upload
+* @property SS_URI $uri
+* @property SS_User_agent $user_agent
+* @property SS_Validation $validation
+* @property SS_Xmlrpc $xmlrpc
+* @property SS_Xmlrpcs $xmlrpcs
+* @property SS_Zip $zip
+*/
+
+class SS_Controller {};
+
+/**
+* @property SS_DB_active_record $db
+* @property SS_DB_forge $dbforge
+* @property SS_Config $config
+* @property SS_Loader $load
+* @property SS_Session $session
+*/
+
+class SS_Model {};
+?> 
