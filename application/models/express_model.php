e60622a39c7998f559f1b0cf93bb654c19f03bf6
陆秋石 <uicestone@gmail.com>
2013-03-05 00:31:05 +0800

    fix: 快递模型

--- application/models/express_model.php
+++ application/models/express_model.php
@@ -4,23 +4,15 @@ class Express_model extends SS_Model{
 		parent::__construct();
 	}
 
-	function fetch($id){
-            $query="
-				SELECT * 
-				FROM express 
-				WHERE id = '{$id}' AND company='{$this->company->id}'";
-            $result=$this->db->query($query)->result_array();
-            return $result[0];
-	}
-	
 	function getList(){
 		$query="
-			SELECT express.id,express.destination,express.content,express.comment,express.time_send,express.num,staff.name AS sender_name
-			FROM express LEFT JOIN staff ON staff.id=express.sender
-			WHERE express.display=1
+			SELECT express.id,express.destination,express.content,express.comment,express.time_send,express.num,
+				people.name AS sender_name
+			FROM express LEFT JOIN people ON people.id=express.sender
+			WHERE express.company={$this->company->id} AND express.display=1
 		";
 		
-		$query=$this->search($query,array('num'=>'单号','staff.name'=>'寄送人','destination'=>'寄送地点'));//为当前sql对象添加搜索条件
+		$query=$this->search($query,array('num'=>'单号','people.name'=>'寄送人','destination'=>'寄送地点'));//为当前sql对象添加搜索条件
 		$query=$this->orderBy($query,'time_send','DESC');//为当前sql对象添加orderby从句
 		$query=$this->pagination($query);//为当前sql对象添加limit从句
 
