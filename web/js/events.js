2777447f5282522f3f5c908ed5c226a5d0ec0f34
陆秋石 <uicestone@gmail.com>
2013-05-14 15:02:23 +0800

    fix: 可能导致非webkit js崩溃的错误

--- web/js/events.js
+++ web/js/events.js
@@ -61,7 +61,7 @@ $(document)
 	});
 	
 	header.on('blockload','.new-messages:not(:empty)',function(){
-		if (window.webkitNotifications.checkPermission() === 0) {
+		if (window.webkitNotifications && window.webkitNotifications.checkPermission() === 0) {
 			window.webkitNotifications.createNotification('/images/favicon.ico', sysname,'您有新的消息').show();
 		}
 		
